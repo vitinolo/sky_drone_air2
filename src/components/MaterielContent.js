293bf7516e9354ce57f93da31b447f4c12f668ed
thibaut lenoir <lenoir677@gmail.com>
2022-12-07 16:38:44 +0100

    responsive:home, media, contact, tarifs, materiel

--- src/components/MaterielContent.js
+++ src/components/MaterielContent.js
@@ -3,30 +3,22 @@ import React from 'react';
 const MaterielContent = () => {
     return (
         <div className='materielcontent'>
-            <div className='materiel'>
-                <div className='descrip'>
-                    <div className='wrap'>
-                        <img src="medias/img/materiel/poste_travail.jpg" alt="poste de travail informatique"></img>
-                        <span>Matériel informatique iMac et iPad.</span>
-                    </div>
+            <div className='materiel'>    
+                <div className='wrap'>
+                    <img src="medias/img/materiel/poste_travail.jpg" alt="poste de travail informatique"></img>
+                    <span>Matériel informatique iMac et iPad.</span>
                 </div>
-                <div className='descrip'>
-                    <div className='wrap'>
-                        <span>Drônes de marque Dji.</span>
-                        <img src="medias/img/materiel/drone.jpg" alt="poste de travail informatique"></img>
-                    </div>
+                <div className='wrap'>
+                    <span>Drônes de marque Dji.</span>
+                    <img src="medias/img/materiel/drone.jpg" alt="poste de travail informatique"></img>
                 </div>
-                <div className='descrip'>
-                    <div className='wrap'>
-                        <img src="medias/img/materiel/casque.jpg" alt="poste de travail informatique"></img>
-                        <span>Casque de marque Dji.</span>
-                    </div>
+                <div className='wrap'>
+                    <img src="medias/img/materiel/casque.jpg" alt="poste de travail informatique"></img>
+                    <span>Casque de marque Dji.</span>
                 </div>
-                <div className='descrip'>
-                    <div className='wrap'>
-                        <span>GoPro sur stabilisateur.</span>
-                        <img src="medias/img/materiel/Gopro.jpg" alt="poste de travail informatique"></img>
-                    </div>
+                <div className='wrap'>
+                    <span>GoPro sur stabilisateur.</span>
+                    <img src="medias/img/materiel/Gopro.jpg" alt="poste de travail informatique"></img>
                 </div>
             </div>
             <div className='logiciel'>
