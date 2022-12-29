a025a04a9703ac321c7c198848434f13b903eca7
thibaut lenoir <lenoir677@gmail.com>
2022-12-29 09:12:06 +0100

    teaser

--- src/components/VideoTeaser.js
+++ src/components/VideoTeaser.js
@@ -4,7 +4,7 @@ function VideoTeaser() {
     return (
         <>
             <div className='video'>
-                <iframe width="915" height="515" src="https://www.youtube.com/embed/b2MOk3MwZxI?autoplay=1" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <iframe width="915" height="515" src="https://www.youtube.com/embed/VpHPaAHgYOY" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>
         </>
     );
