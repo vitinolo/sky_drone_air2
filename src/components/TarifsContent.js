e1b1b79f1ce22d73f990423a35765adc3ba31290
thibaut lenoir <lenoir677@gmail.com>
2023-01-14 11:17:01 +0100

    suppression pdf

--- src/components/TarifsContent.js
+++ src/components/TarifsContent.js
@@ -5,11 +5,7 @@ const TarifsContent = () => {
         <div className='tarifscontent'>
             <div className='devisWrapper'>
                 <h1>Devis et tarifs sur demande</h1>
-                <p>Ouvrir le devis en PDF
-                    <a href="./medias/devis/devis.pdf" title='ouvrir le devis en pdf'> Devis</a>.
-                </p>
                 <span>Veuillez nous contacter pour parfaire votre projet.</span>
-                <img alt="devis" src="./medias/devis/devis.webp"></img>
                 <img alt="tarifs" src="./medias/devis/tarifs.webp"></img>
             </div>
         </div>
