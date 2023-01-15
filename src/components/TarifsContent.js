f26fdc0627079b2993ca8bc40d80439aba212e55
thibaut lenoir <lenoir677@gmail.com>
2023-01-15 17:39:54 +0100

    img tarifs

--- src/components/TarifsContent.js
+++ src/components/TarifsContent.js
@@ -5,7 +5,6 @@ const TarifsContent = () => {
         <div className='tarifscontent'>
             <div className='devisWrapper'>
                 <h1>Devis et tarifs sur demande</h1>
-                <span>Veuillez nous contacter pour parfaire votre projet.</span>
                 <img alt="tarifs" src="./medias/devis/tarifs.webp"></img>
             </div>
         </div>
